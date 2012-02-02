38ce959c82f25889261451a48ff10bad0dcbcf6a
Ramiro Jr. Franco <rjfranco@gmail.com>
2012-02-02 13:10:53 -0800

    Adding initial files to the backbone rails 3.2 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneMyGames::Application.config.secret_token = '0244e369fc93b549f169b08ef84cf75c3fd5ad425b7ba9ff877bc3150f50dfea9f5a75db9320099fec089eecdb03306e81530dc9dd80c56b1f75297403d2fe48'
